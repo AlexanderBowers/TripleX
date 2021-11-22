d3e0a34c73194bdc15d6d7ca62431f0d09185ecf
Alexander <7319abowers@gmail.com>
2021-11-21 20:57:36 -0800

    renamed variables

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -6,13 +6,16 @@ int main()
 	cout << "You have found a large chest with a lock on it.\n";
 	cout << "Decipher the clues to find the prize inside...\n";
 
-	int a = 4;
-	int b = 3;
-	int c = 2;
-	int sum = a + b + c;
-	int product = a * b * c;
-	cout << sum;
-	cout << "product is: " << product;
+	int CodeA = 4;
+	int CodeB = 3;
+	int CodeC = 2;
+	int CodeSum = CodeA + CodeB + CodeC;
+	int CodeProduct = CodeA * CodeB * CodeC;
+	
+	cout << endl;
+	cout << "* There are three numbers\n";
+	cout << "* The numbers add up to: " << CodeSum << endl;
+	cout << "* The numbers multiply to give: " << CodeProduct << endl;
 
 	return 0;
 }
