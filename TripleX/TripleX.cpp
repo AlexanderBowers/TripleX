e4142faa920451008d52cc03a8352bcc7a2e521f
Alexander <7319abowers@gmail.com>
2021-11-21 20:30:11 -0800

    added intro message

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+using namespace std;
+
 int main()
 {
+	cout << "You have found a large chest with a lock on it.\n";
+	cout << "Decipher the clues to find the prize inside...\n";
 	return 0;
 }
