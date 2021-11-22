361d5b061fdfc241aa3cc3da71eacd42dbfb658f
Alexander <7319abowers@gmail.com>
2021-11-21 20:40:52 -0800

    learned arrithmatic

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -5,5 +5,14 @@ int main()
 {
 	cout << "You have found a large chest with a lock on it.\n";
 	cout << "Decipher the clues to find the prize inside...\n";
+
+	int a = 4;
+	int b = 3;
+	int c = 2;
+	int sum = a + b + c;
+	int product = a * b * c;
+	cout << sum;
+	cout << "product is: " << product;
+
 	return 0;
 }
